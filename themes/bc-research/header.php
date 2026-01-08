0c5757ecf3247fb5de44d8dee659d9c2f829c3f5
Celine Wang <congling00369@gmail.com>
2026-01-08 03:06:01 -0800

    get header menu part done.

--- themes/bc-research/header.php
+++ themes/bc-research/header.php
@@ -30,7 +30,7 @@ if (!defined('ABSPATH')) exit;
             <li class="menu-item"><a href="/resources">Resources</a></li>
             <li class="menu-item"><a href="/news">News</a></li>
             <li class="menu-item"><a href="/contact">Contact</a></li>
-            <li class="menu-item"><a class="utility-icon" href="https://www.linkedin.com">in</a></li>
+            <li class="menu-item"><a class="utility-icon" href="https://www.linkedin.com" aria-label="LinkedIn">in</a></li>
           </ul>
         <?php endif; ?>
       </nav>
