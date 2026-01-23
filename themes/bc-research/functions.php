84db55cdd69254514eab91f0e20bee4d7b28fb47
Celine Wang <congling00369@gmail.com>
2026-01-23 10:37:08 -0800

    add analytical laboratory logic to the function.php file to connect to the filter bar section for the equipment display.

--- themes/bc-research/functions.php
+++ themes/bc-research/functions.php
@@ -22,6 +22,7 @@ add_action('wp_enqueue_scripts', function () {
   $theme_dir = get_template_directory();
   $theme_uri = get_template_directory_uri();
 
+  // Vendor
   wp_enqueue_style(
     'bootstrap-icons',
     'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
@@ -29,6 +30,7 @@ add_action('wp_enqueue_scripts', function () {
     '1.11.3'
   );
 
+  // Global CSS
   wp_enqueue_style(
     'bc-research-style',
     $theme_uri . '/assets/css/main.css',
@@ -36,6 +38,7 @@ add_action('wp_enqueue_scripts', function () {
     $ver
   );
 
+  // Front page CSS
   if (is_front_page()) {
     wp_enqueue_style(
       'bc-research-front-page',
@@ -45,10 +48,19 @@ add_action('wp_enqueue_scripts', function () {
     );
   }
 
+  /**
+   * Page-specific CSS + JS by slug
+   * Example:
+   *  /analytical-laboratory/ will load:
+   *   - /assets/css/page-analytical-laboratory.css (if exists)
+   *   - /assets/js/page-analytical-laboratory.js  (if exists)
+   */
   if (is_page()) {
-    $page_id = get_queried_object_id();
+    $page_id   = get_queried_object_id();
     $page_slug = $page_id ? get_post_field('post_name', $page_id) : '';
+
     if (!empty($page_slug)) {
+      // Page CSS
       $page_css_rel = '/assets/css/page-' . $page_slug . '.css';
       if (file_exists($theme_dir . $page_css_rel)) {
         wp_enqueue_style(
@@ -58,9 +70,22 @@ add_action('wp_enqueue_scripts', function () {
           $ver
         );
       }
+
+      // Page JS  (ADDED)
+      $page_js_rel = '/assets/js/page-' . $page_slug . '.js';
+      if (file_exists($theme_dir . $page_js_rel)) {
+        wp_enqueue_script(
+          'bc-research-page-' . $page_slug,
+          $theme_uri . $page_js_rel,
+          [],     // add dependencies here if needed (e.g., ['jquery'])
+          $ver,
+          true
+        );
+      }
     }
   }
 
+  // Global JS
   wp_enqueue_script(
     'bc-research-js',
     $theme_uri . '/assets/js/main.js',
